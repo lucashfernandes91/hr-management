9173f2fcc707b99a997256d5981eeb8a11323c83
Lucas Fernandes <lucashfernandes@yahoo.com.br>
2024-10-06 23:38:35 -0300

    Criação das models part-1

--- apps/employees/admin.py
+++ apps/employees/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Enterprise
+from .models import Employee
 
-admin.site.register(Enterprise)
+admin.site.register(Employee)
--- apps/overtime_record/models.py
+++ apps/overtime_record/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.forms import ModelForm
 
 
 class OvertimeRecord(models.Model):
-    pass
+    reason = models.CharField(max_length=100, help_text="Qual o motivo para a hora extra?")
+    hours = models.CharField(max_length=5, help_text="Quantidade de horas trabalhadas?")
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
