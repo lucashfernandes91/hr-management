12d33c3474927542c665bbf49226cf309ebe04ee
Lucas Fernandes <lucashfernandes@yahoo.com.br>
2024-10-08 01:46:21 -0300

    Ajutes nas models e, nfiguração do template e pagina de login

--- apps/employees/models.py
+++ apps/employees/models.py
@@ -2,16 +2,18 @@ from django.db import models
 
 
 class Employee(models.Model):
-    name = models.CharField(max_length=70, help_text="Nome do colaborador")
-    email = models.CharField(max_length=80, help_text="E-mail de contato")
-    phone = models.CharField(max_length=15, help_text="Telefone de contato")
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
+	name = models.CharField(max_length=70, verbose_name="Nome", help_text="Nome do colaborador")
+	email = models.CharField(max_length=80, verbose_name="E-mail", help_text="E-mail de contato")
+	phone = models.CharField(max_length=15, verbose_name="Telefone", help_text="Telefone de contato")
+	created_at = models.DateTimeField(auto_now_add=True)
+	modified_at = models.DateTimeField(auto_now=True)
 
 
-    class Meta:
-        ordering = ['name']
+	class Meta:
+		ordering = ['name']
+		verbose_name = "Colaborador"
+		verbose_name_plural = "Colaboradores"
 
 
-    def __str__(self):
-        return self.name
+	def __str__(self):
+		return self.name
